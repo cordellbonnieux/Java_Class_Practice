106c601f6c8a01447a659e0207c8f1c892cc7971
Cordell <cordellbonnieux@gmail.com>
2021-08-11 16:10:33 -0700

    Fixed BinarySearch and connected it with Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,28 +1,25 @@
 public class BinarySearch {
 
-    int[] search(int[] arr, int key) {
+    int search(int[] arr, int key) {
 
-        int mid = arr.length / 2;
+        int left = 0,
+            right = arr.length - 1;
+        
+        while (left <= right) {
 
-        if (key == arr[mid])
-            return mid;
+            int middle = (left + right) / 2;
 
-        else if (key > mid) {
-            int[] rightHalf = new int[(arr.length - mid)];
-            for (int i = 0, j = arr[mid + 1]; i < rightHalf.length; i++, j++) {
-                rightHalf[i] = arr[j];
-            }
-            return search(rightHalf, key);
-        } 
+            if (key < arr[middle])
+                right = middle - 1;
+            
+            else if (key > arr[middle])
+                left = middle + 1;
+            
+            else  
+                return middle;
+        }
 
-        else if (key < mid) {
-            int[] leftHalf = new int[mid];
-            for (int i = 0; i < leftHalf.length; i++) {
-                leftHalf[i] = arr[i];
-            }
-            return search(leftHalf, key);
-        } 
+        return -1;
 
     }
-
 }
---
+++ Search.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Search {
+
+    public static void main(String[] args) {
+
+        int[] list = {0,1,3,4,5,7,12,18};
+
+        BinarySearch binary = new BinarySearch();
+
+        System.out.println(binary.search(list, 12));
+
+    }
+}
