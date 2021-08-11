0ffbaa3a4dfc19f6f9774b46d0596b4ff6f8c969
Cordell <cordellbonnieux@gmail.com>
2021-08-11 16:45:26 -0700

    Added optionsPanel() which creates a j panel with the taks the program can perform. Also moved the list entry into it's own method.

--- Reverse.java
+++ Reverse.java
@@ -29,5 +29,6 @@ public class Reverse {
 
         System.out.println("Backwards: " + reverse);
 
+        read.close();
     }
 }
--- Search.java
+++ Search.java
@@ -1,14 +1,83 @@
 import java.util.*;
+import javax.swing.*;
 
 public class Search {
 
     public static void main(String[] args) {
 
-        int[] list = {0,1,3,4,5,7,12,18};
-
+        // create search objects
         BinarySearch binary = new BinarySearch();
 
-        System.out.println(binary.search(list, 12));
+        // create options panel
+        JPanel options = optionsPanel();
 
+        // Ask the user what they'd like to do
+        String choice = JOptionPane.showInputDialog(null, options);
+
+        switch (choice) {
+            
+        }
+
+
+
+
+        //System.out.println(binary.search(list, 12));
+
+    }
+
+    /**
+     * A method for creating the options panel
+     * @return returns a j panel with all the tasks the user can perform
+     */
+    public static JPanel optionsPanel() {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        JPanel c = new JPanel();
+        panel.add(c);
+
+        JLabel choose = new JLabel("What would you like to do?");
+        c.add(choose);        
+
+        JLabel option1 = new JLabel("Enter a new list: 1");
+        c.add(option1);
+
+        JLabel option2 = new JLabel("View current list: 2");
+        c.add(option2);
+
+        JLabel option3 = new JLabel("Quick Sort: 3");
+        c.add(option3);
+
+        JLabel option4 = new JLabel("Binary Search: 4");
+        c.add(option4);
+
+        JLabel option5 = new JLabel("Linear Search: 5");
+        c.add(option5);
+
+        return panel;
+    }
+
+    /**
+     * Gets a list of integers from the user
+     * @return an array of integers
+     */
+    public static int[] getList() {
+
+        // get user input
+        String input = JOptionPane.showInputDialog(null, "Enter a list of integers separated by commas (,): ");
+
+        // convert input to an array of integers
+        String[] string = input.split(",");
+
+        int[] list = new int[string.length];
+
+        for (int i = 0; i < string.length; i++)
+            try {
+                list[i] = Integer.parseInt(string[i]);
+            } catch (Exception e) {
+                System.out.println("Error parsing string");
+            }
+        
+        return list;
     }
 }
